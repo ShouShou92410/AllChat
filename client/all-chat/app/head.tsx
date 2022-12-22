6955ab055777bddf115f0507e0603b5be5ab338c
Sean Wen <sw92410@hotmail.com>
2022-12-22 14:30:30 -0700

    Upgrade ui to use Nextjs's app dir feature. - Add app folder - Delete pages folder - Move styles in app folder

---
+++ client/all-chat/app/head.tsx
@@ -0,0 +1,10 @@
+import './(styles)/globals.css';
+
+export default function Head() {
+	return (
+		<>
+			<title>All Chat</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+		</>
+	);
+}
---
+++ client/all-chat/app/page.tsx
@@ -0,0 +1,5 @@
+import styles from './(styles)/styles.module.css';
+
+export default function Page() {
+	return <main className={styles.main}>Chat here</main>;
+}
