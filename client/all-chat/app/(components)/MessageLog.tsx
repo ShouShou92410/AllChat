fd59dc1d8d001208085d92143a09c76e43f4822d
Sean Wen <sw92410@hotmail.com>
2022-12-28 17:45:11 -0700

    Basic message history logic with scrolling - Add custom hook for fetching old messages - Fix the order which the messages are shown - Fetch message on first render and when scroll to the top

--- client/all-chat/app/(components)/MessageLog.tsx
+++ client/all-chat/app/(components)/MessageLog.tsx
@@ -1,16 +1,25 @@
 'use client';
 
-import { useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { IChatPayload, WebSocketContext } from '../(context)/WebSocketContext';
 import MessageItem from './MessageItem';
 
 const MessageLog = () => {
+	// Message history
+	const { data, isLoading, fetchMessage } = useGetMessage();
+	useEffect(() => {
+		fetchMessage();
+	}, []);
+	useEffect(() => {
+		if (data && !isLoading) {
+			setMessages([...messages, ...data]);
+		}
+	}, [data, isLoading]);
 	const handleScroll = (e) => {
 		const scrollPosition = e.target.scrollHeight + e.target.scrollTop - e.target.clientHeight; // Because of flex-col-reverse, scrollTop is negative
 
 		if (scrollPosition === 0) {
-			console.log('fetech');
-			e.target.scrollTop = 1000 - e.target.scrollHeight;
+			fetchMessage();
 		}
 	};
 
@@ -20,7 +29,7 @@ const MessageLog = () => {
 	useEffect(() => {
 		if (isConnected) {
 			if (payload?.type === 'client') {
-				setMessages([...messages, payload.data]);
+				setMessages([payload.data, ...messages]);
 				console.log(payload);
 			}
 		}
@@ -41,3 +50,33 @@ const MessageLog = () => {
 };
 
 export default MessageLog;
+
+const useGetMessage = () => {
+	const [limit, setLimit] = useState(Math.ceil(window.innerHeight / 100)); // Approximating the amount of message shown on the screen
+	const [last, setLast] = useState('');
+
+	const [data, setData] = useState<any>(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const fetchMessage = useCallback(async () => {
+		if (last !== undefined) {
+			setIsLoading(true);
+			setData(null);
+
+			const res = await fetch(`http://localhost:3001/message?limit=${limit}&last=${last}`);
+
+			if (!res.ok) {
+				// This will activate the closest `error.js` Error Boundary
+				throw new Error('Failed to fetch data');
+			}
+
+			const result = await res.json();
+
+			setLast(result.last);
+			setData(result.items);
+			setIsLoading(false);
+		}
+	}, [limit, last]);
+
+	return { data, isLoading, fetchMessage };
+};
