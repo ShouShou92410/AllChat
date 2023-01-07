6beb3664ba34372291a8f634c83f84f4406006ad
Sean Wen <sw92410@hotmail.com>
2023-01-06 22:32:13 -0700

    Try adding navbar

--- client/all-chat/app/layout.tsx
+++ client/all-chat/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: IProps) {
 			<body className="min-h-screen bg-slate-300 dark:bg-slate-900 dark:text-slate-300">
 				<WebsocketProvider>
 					<ServerNotification />
+					<div className="absolute w-screen h-12 border-b dark:bg-slate-900">TEST</div>
 					<main className="min-h-screen max-h-screen w-3/5 m-auto bg-slate-100 dark:bg-slate-800">
 						{children}
 					</main>
